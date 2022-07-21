68d82d09626fb551d2b20f60e39ad14c044db04e
Fukaya Temma <23367170+Pranc1ngPegasus@users.noreply.github.com>
2022-07-21 20:49:40 +0900

    idp practice (#1)
    * go mod init
    
    * Google Identity Platform practice

---
+++ idp-practice/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+
+	firebase "firebase.google.com/go/v4"
+	"firebase.google.com/go/v4/auth"
+)
+
+func main() {
+	ctx := context.Background()
+
+	gcpProjectID := os.Getenv("GCP_PROJECT_ID")
+	email := os.Getenv("EMAIL")
+
+	app, err := firebase.NewApp(ctx, &firebase.Config{
+		ProjectID: gcpProjectID,
+	})
+	if err != nil {
+		log.Fatalf("error initializing app: %v\n", err)
+	}
+
+	client, err := app.Auth(ctx)
+	if err != nil {
+		log.Fatalf("error getting Auth client: %v\n", err)
+	}
+
+	users, err := client.GetUsers(ctx, []auth.UserIdentifier{
+		auth.EmailIdentifier{Email: email},
+	})
+	if err != nil {
+		log.Fatalf("error getting users: %+v\n", err)
+	}
+
+	log.Printf("Successfully fetched user data")
+	for _, u := range users.Users {
+		log.Printf("%+v", u.UID)
+	}
+}
